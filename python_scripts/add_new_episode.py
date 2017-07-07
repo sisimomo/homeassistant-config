38db7af8beefc796041952f877a44746b9d383e3
PhyberApex <phyber.apex@gmail.com>
2017-07-08 01:12:54 +0200

    - Fixed init automation - Github #38

--- python_scripts/add_new_episode.py
+++ python_scripts/add_new_episode.py
@@ -5,6 +5,7 @@ if ep_name is None:
 else:
     current_state = hass.states.get("sensor.recent_episodes")
     if current_state is None:
-      hass.states.set(hass.states.set("sensor.recent_episodes", "INIT"))
-    new_attributes = {'friendly_name': 'Recent entries in Emby', 'entry1': ep_name, 'entry2':current_state.attributes.get('entry1') or 'NO ENTRY', 'entry3':current_state.attributes.get('entry2') or 'NO ENTRY', 'entry4':current_state.attributes.get('entry3') or 'NO ENTRY', 'entry5':current_state.attributes.get('entry4')}
-    hass.states.set("sensor.recent_episodes", ep_name, attributes=new_attributes)
+      hass.states.set(hass.states.set("sensor.recent_episodes", True, 'NO ENTRY'))
+    else:
+      new_attributes = {'friendly_name': 'Recent entries in Emby', 'entry1': ep_name, 'entry2':current_state.attributes.get('entry1') or 'NO ENTRY', 'entry3':current_state.attributes.get('entry2') or 'NO ENTRY', 'entry4':current_state.attributes.get('entry3') or 'NO ENTRY', 'entry5':current_state.attributes.get('entry4')}
+      hass.states.set("sensor.recent_episodes", ep_name, attributes=new_attributes)
