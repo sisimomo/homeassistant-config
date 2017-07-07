26ba0fd387b4f2efa838b04eb8a3a4044ea14da7
PhyberApex <phyber.apex@gmail.com>
2017-07-08 01:26:43 +0200

    - Fixed init automation - Github #38

--- python_scripts/add_new_episode.py
+++ python_scripts/add_new_episode.py
@@ -4,7 +4,7 @@ if ep_name is None:
     logger.error("Parameter: 'info' missing please add it to the call")
 else:
     current_state = hass.states.get("sensor.recent_episodes")
-    if ep_name is 'INIT':
+    if ep_name == 'INIT':
       new_attributes = {'friendly_name': 'Recent entries in Emby', 'entry1': 'NO ENTRY', 'entry2': 'NO ENTRY', 'entry3': 'NO ENTRY', 'entry4': 'NO ENTRY', 'entry5': 'NO ENTRY'}
       hass.states.set(hass.states.set("sensor.recent_episodes", 'NO ENTRY', attributes))
     else:
