d9d94fadabf1067c7157bbb6d17f371eb0cd8742
PhyberApex <phyber.apex@gmail.com>
2017-08-03 00:18:07 +0200

    - Fixed same error in new script - Handling of state object - Github #93

--- python_scripts/floorplan_alarm.py
+++ python_scripts/floorplan_alarm.py
@@ -3,8 +3,8 @@ number = data.get('number')
 action = data.get('action')
 if action is None:
     pin = hass.states.get("sensor.floorplan_alarm_pin")
-    new_pin = pin + "" + number
-    hass.states.set(hass.states.set("sensor.floorplan_alarm_pin", new_pin))
+    new_pin = pin.state + "" + number
+    hass.states.set("sensor.floorplan_alarm_pin", new_pin)
 else:
     pin = hass.states.get("sensor.floorplan_alarm_pin")
-    hass.services.call(alarm_control_panel, action, service_data={ 'entity_id': 'alarm_control_panel.alarm', 'code': pin })
+    hass.services.call(alarm_control_panel, action, service_data={ 'entity_id': 'alarm_control_panel.alarm', 'code': pin.state })
