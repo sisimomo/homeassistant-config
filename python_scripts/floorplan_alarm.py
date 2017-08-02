a232c6b63263e3dd9e7cd5c87b09678870b12982
PhyberApex <phyber.apex@gmail.com>
2017-08-03 00:26:53 +0200

    - Added clear action - Github #93

--- python_scripts/floorplan_alarm.py
+++ python_scripts/floorplan_alarm.py
@@ -2,12 +2,14 @@
 number = data.get('number')
 action = data.get('action')
 if action is None:
-    pin = hass.states.get("sensor.floorplan_alarm_pin")
+    pin = hass.states.get('sensor.floorplan_alarm_pin')
     if pin.state == 'unknown':
         new_pin = number
     else:
         new_pin = pin.state + number
-    hass.states.set("sensor.floorplan_alarm_pin", new_pin)
+    hass.states.set('sensor.floorplan_alarm_pin', new_pin)
+elif action == 'clear':
+    hass.states.set("sensor.floorplan_alarm_pin", '')
 else:
-    pin = hass.states.get("sensor.floorplan_alarm_pin")
+    pin = hass.states.get('sensor.floorplan_alarm_pin')
     hass.services.call(alarm_control_panel, action, service_data={ 'entity_id': 'alarm_control_panel.alarm', 'code': pin.state })
