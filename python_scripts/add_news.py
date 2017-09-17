14d6cc31538868cc5ec84b9901358ff185968bc6
PhyberApex <phyber.apex@gmail.com>
2017-09-17 20:06:20 +0200

    - Removed sensors for news - Added new text_inputs for news - Changed script to update new text_input instead of sensors - Github #101

--- python_scripts/add_news.py
+++ python_scripts/add_news.py
@@ -1,12 +1,11 @@
 #info needs to be available
-ep_name = data.get('info')
-if ep_name is None:
+news = data.get('info')
+if news is None:
     logger.error("Parameter: 'info' missing please add it to the call")
 else:
-    current_state = hass.states.get('sensor.news')
-    if ep_name == 'INIT':
-      new_attributes = {'friendly_name': 'Five latest news', 'entry1': 'NO ENTRY', 'entry2': 'NO ENTRY', 'entry3': 'NO ENTRY', 'entry4': 'NO ENTRY', 'entry5': 'NO ENTRY'}
-      hass.states.set('sensor.news', 'NO ENTRY', new_attributes)
-    else:
-      new_attributes = {'friendly_name': 'Five latest news', 'entry1': ep_name, 'entry2':current_state.attributes.get('entry1'), 'entry3':current_state.attributes.get('entry2'), 'entry4':current_state.attributes.get('entry3'), 'entry5':current_state.attributes.get('entry4')}
-      hass.states.set('sensor.news', ep_name, attributes=new_attributes)
+    # Switch states around
+    for x in range(1, 5):
+        state = hass.states.get('input_text.news'+x).state
+        hass.states.set('input_text.news'+(x+1), old_state)
+    # Set new state
+    hass.states.set('input_text.news1', news)
