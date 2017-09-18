337a0261fabd6fed0bb177e8148a6093ab7c377d
PhyberApex <phyber.apex@gmail.com>
2017-09-18 13:25:45 +0200

    - Fixed script to add news correctly - Github #101

--- python_scripts/add_news.py
+++ python_scripts/add_news.py
@@ -4,7 +4,7 @@ if news is None:
     logger.error("Parameter: 'info' missing please add it to the call")
 else:
     # Switch states around
-    for x in range(1, 5):
+    for x in range(4, 0, -1):
         switchState = hass.states.get("input_text.news"+str(x)).state
         hass.states.set('input_text.news'+str((x+1)), switchState)
     # Set new state
